1e1d582842aa7c41288c711bedb30c6da33a0415
Simon Okutan <90206664+Nomiez@users.noreply.github.com>
2023-11-28 02:17:42 +0100

    Fix proxy for docker compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,23 @@
-import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
+import {defineConfig, loadEnv} from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '', // Empty string leads to vite generating relative URL paths during build
-    plugins: [preact()],
-    server: {
-        proxy: {
-            // Proxy all requests to /<objectId>/api/... to http://localhost:8080/<objectId>/api/... (CORS)
-            '/api': {
-                target: 'http://0.0.0.0:8000',
-                changeOrigin: true,
-                rewrite: path => path.replace(/^\/api/, ''),
+export default ({mode}) => {
+
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+    return defineConfig({
+
+        base: '', // Empty string leads to vite generating relative URL paths during build
+        plugins: [preact()],
+        server: {
+            proxy: {
+                // Proxy all requests to /<objectId>/api/... to http://localhost:8080/<objectId>/api/... (CORS)
+                '/api': {
+                    target: process.env.VITE_PROXY_API_URL || 'server:8080',
+                    changeOrigin: true,
+                    rewrite: path => path.replace(/^\/api/, ''),
+                },
             },
         },
-    },
-})
+    });
+};
