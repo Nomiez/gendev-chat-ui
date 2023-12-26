370ad93b7499bbdf783ef4dd64b2ec00fa9bd106
Simon Okutan <90206664+Nomiez@users.noreply.github.com>
2023-12-26 21:16:44 +0100

    Fix proxy configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,18 @@
 import preact from '@preact/preset-vite'
-import {defineConfig, loadEnv} from 'vite'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
-
-    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
-    return defineConfig({
-
-        base: '', // Empty string leads to vite generating relative URL paths during build
-        plugins: [preact()],
-        server: {
-            proxy: {
-                // Proxy all requests to /<objectId>/api/... to http://localhost:8080/<objectId>/api/... (CORS)
-                '/api': {
-                    target: process.env.VITE_PROXY_API_URL || 'http://server:8080',
-                    changeOrigin: true,
-                    rewrite: path => path.replace(/^\/api/, ''),
-                },
+export default defineConfig({
+    base: '', // Empty string leads to vite generating relative URL paths during build
+    plugins: [preact()],
+    server: {
+        proxy: {
+            // Proxy all requests to /<objectId>/api/... to http://localhost:8080/<objectId>/api/... (CORS)
+            '/api': {
+                target: "http://0.0.0.0:8000",
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/api/, ''),
             },
         },
-    });
-};
+    },
+});
